c2e986009f3adeb2243a2cac53f3ab2f97cc68ee
Kubik161 <30665071+Kubik161@users.noreply.github.com>
2026-05-22 10:06:52 +0200

    cors PUT fix (#274)

--- orca/src/server/cors.rs
+++ orca/src/server/cors.rs
@@ -18,7 +18,7 @@ impl Fairing for Cors {
             response.set_status(Status::NoContent);
             response.set_header(Header::new(
                 "Access-Control-Allow-Methods",
-                "POST, PATCH, GET, DELETE",
+                "POST, PUT, PATCH, GET, DELETE",
             ));
             response.set_header(Header::new(
                 "Access-Control-Allow-Headers",
