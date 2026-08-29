2a062bd4be098d08ffafb9c185336c2cef760726
Kubik161 <30665071+Kubik161@users.noreply.github.com>
2026-08-29 11:29:01 +0200

    nixpkgs update (#303)

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "ITC Union application and member management system";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     flake-utils.url = "github:numtide/flake-utils";
     crane.url = "github:ipetkov/crane";
     rust-overlay.url = "github:oxalica/rust-overlay";
